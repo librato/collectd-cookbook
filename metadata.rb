019c84867d51b1a1b52ddbd2b1a0f4de2c77c291
Mike Heffner <mikeh@fesnel.com>
2012-01-22 22:20:03 -0500

    Bump patch version.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "nkantrowitz@crypticstudios.com"
 license          "Apache 2.0"
 description      "Install and configure the collectd monitoring daemon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "1.0.2"
+version          "1.0.3"
 supports         "ubuntu"
 
 recipe "collectd", "Installs collectd standalone"
